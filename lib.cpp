54f7f3b4a09f60581a8bdd1c8e0e29bf37e848b0
stanislavkuskov <kuskov.stanislav@gmail.com>
2019-10-20 13:46:33 +0300

    CI for ip addresses filtering

---
+++ lib.cpp
@@ -0,0 +1,12 @@
+//
+// Created by kuskov on 20.10.2019.
+//
+
+#include "lib.h"
+
+#include "version.h"
+
+int version()
+{
+    return PROJECT_VERSION_PATCH;
+}
---
+++ test_version.cpp
@@ -0,0 +1,16 @@
+//
+// Created by kuskov on 20.10.2019.
+//
+
+#include "lib.h"
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(ip_filter_test_suite)
+
+    BOOST_AUTO_TEST_CASE(ip_filter_test_suite)
+    {
+        BOOST_CHECK(version() > 0);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
